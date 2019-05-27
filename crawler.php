679dcf56b86568e2f0fe3ad5c9048cd6c8427981
tonderaimuchada <tonderaimuchada@gmail.com>
2019-05-27 17:21:57 +0200

    Reasoning functionality

--- crawler.php
+++ crawler.php
@@ -1,15 +1,21 @@
 <?php
-	$searchValue = 'tes';
+	$searchValue = 'test';
 
 	define('ROOTPATH', __DIR__);
 	$inTags = true;
+	$new = true;
 	$dir = ROOTPATH.'/SW';
 	$htmlFiles = array();
+	include 'reasoner.php';
+	$urlList = [];
 
     foreach (readFileSubDir($dir) as $fileItem) {
 		if($myfile = @fopen($fileItem, 'r+')){
 			while (($buffer = fgets($myfile)) !== false) {
 				if (strpos($buffer, $searchValue) !== false) {
+					//echo("File name: ".$fileItem);
+
+					array_push($urlList, $fileItem);
 					if(strpos($buffer, $searchValue) == 0){
 						$inTags = false;
 					} else {
@@ -30,30 +36,65 @@
 					}
 					// echo($buffer);
 					// echo(strpos($buffer, $searchValue)."\n");
-					echo($fileItem."\n")."\n";
+					
+					// if($new){
+					// 	fwrite($txtFile,"1|".$fileItem);
+					// } else {
+					// 	// Update the rating
+					// 	$rating = substr($contents, 0, strpos($contents, '|'));
+					// 	$rating++;
+					// 	echo($rating);
+					// }
+					
+					// echo($fileItem."\n")."\n";
 					break;
 				}
 			}
 			fclose($myfile);
 		}
 	}
+
+	$rankingsFile = "rankings.txt";
+
+	if($txtFile = @fopen($rankingsFile, 'r+')){
+		//$txtFile = @fopen($rankingsFile, "r+") or die("Unable to open file!");
+		// $contents = fread($txtFile,filesize($rankingsFile));
+		// echo($txtBuffer = fgets($txtFile));
+		while (($txtBuffer = fgets($txtFile)) !== false) {
+			foreach($urlList as $fileItem){
+				echo($fileItem);
+				$rating = substr($txtBuffer, 0, strpos($txtBuffer, '|'));
+				// Errors below: Increments not exceeding 2 and not updating the original values
+				if (strpos($txtBuffer, $fileItem) !== false || strpos($txtBuffer, "file://".$fileItem) !== false) {
+					// Update here
+					$new = false;
+					$rating++;
+					fwrite($txtFile,$rating."|".$fileItem."\n");
+				}
+				// else {
+				// 	fwrite($txtFile, "1|".$fileItem."\n");
+				// }
+			}
+		}
+	}
+	fclose($rankingsFile);
 	//print_r($htmlFiles);
 
-    function readFileSubDir($scanDir) {
-    	$handle = opendir($scanDir);
-    	while (($fileItem = readdir($handle)) !== false) {
-    		// skip '.' and '..'
-    		if (($fileItem == '.') || ($fileItem == '..')) continue;
-    		$fileItem = rtrim($scanDir,'/') . '/' . $fileItem;
-    		// if dir found call again recursively
-    		if (is_dir($fileItem)) {
-    			foreach (readFileSubDir($fileItem) as $childFileItem) {
-    				yield $childFileItem;
-    			}
-    		} else {
-    			yield $fileItem;
+  function readFileSubDir($scanDir) {
+    $handle = opendir($scanDir);
+    while (($fileItem = readdir($handle)) !== false) {
+    	// skip '.' and '..'
+    	if (($fileItem == '.') || ($fileItem == '..')) continue;
+    	$fileItem = rtrim($scanDir,'/') . '/' . $fileItem;
+    	// if dir found call again recursively
+    	if (is_dir($fileItem)) {
+    		foreach (readFileSubDir($fileItem) as $childFileItem) {
+    			yield $childFileItem;
     		}
+    	} else {
+    		yield $fileItem;
     	}
-    	closedir($handle);
+    }
+    closedir($handle);
 	}
 ?>
---
+++ reasoner.php
@@ -0,0 +1,19 @@
+<?php
+    $visitsCount = 007;
+    $url;
+    $urlList = [];
+
+    class Reasoner{
+        private $list = [];
+
+        public function getListElements(){
+            return $this->$list;
+        }
+
+        public function incrementElementRating($element){
+            if(array_in($element, $list)){
+
+            }
+        }
+    }
+?>
